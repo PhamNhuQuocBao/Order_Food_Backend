07bf30d9deccc776431ec5a913a8151d9fc559a3
Hanh Tran <tnnganhanh@gmail.com>
2025-05-07 00:52:41 +0700

    update

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ const port = process.env.PORT || 3000;
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 
 // MongoDB Atlas connection string from the .env file
 const mongoUri = process.env.MONGO_URI;
